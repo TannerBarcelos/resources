87379e1ed2dee57d1e13182fd4240e720e914f17
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2025-05-23 14:49:56 -0700

    feat: Implement ChatOpenAI integration and invoke example query

--- ai-ml/ai-agent-tutorial/index.ts
+++ ai-ml/ai-agent-tutorial/index.ts
@@ -1,1 +1,9 @@
-console.log(process.env.OPENAI_API_KEY);
+import { ChatOpenAI } from "@langchain/openai";
+
+const llm = new ChatOpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+    modelName: "gpt-4-1106-preview",
+});
+
+const llmResponse = await llm.invoke("What is the capital of France?");
+console.log(llmResponse);

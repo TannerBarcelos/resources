4578b1b216265115b8b679b64524fe85904dd2dd
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2025-09-30 15:43:21 -0700

    Removing old kafka

--- distributed-systems/kafka/kafka-intro/src/lib/createInstance.ts
+++
@@ -1,8 +0,0 @@
-import { Kafka, KafkaConfig } from 'kafkajs'
-
-function createKafkaInstance(clientId: string, brokers: string[]): Kafka {
-  const kafkaConfig: KafkaConfig = { brokers, clientId }
-  return new Kafka(kafkaConfig)
-}
-
-export default createKafkaInstance
